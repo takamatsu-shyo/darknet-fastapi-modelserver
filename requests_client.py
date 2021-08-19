1f6a9d354075429f97fe39dbb380913a3b8457e2
Sho Yano <takamatsu.shyo@gmail.com>
2021-08-19 14:07:00 +0300

    Cleanup prints

--- requests_client.py
+++ requests_client.py
@@ -15,28 +15,12 @@ import time
 
 t1 = time.process_time()
 img = cv2.resize(cv2.imread('images/frame4.jpg'), (416,416))
-print(sys.getsizeof(img))
 img_encoded = cv2.imencode('.jpg', img)[1]
-print(sys.getsizeof(img_encoded))
-
 img_bytes = img_encoded.tobytes()  # bytes class
-
-print(sys.getsizeof(img_bytes))
-
-# hex string
 img_hex = img_bytes.hex()
-print(type(img_hex))
-print(sys.getsizeof(img_hex))
 
 r = requests.post('http://127.0.0.1:8000/predict/', json={"img": img_hex, "dim": (416,416,3), "model_id" : 0, "save_predictions_on_server": True})
-
 t2 = time.process_time()
 
-# pr.disable()
-# s = io.StringIO()
-# sortby = SortKey.CUMULATIVE
-# ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-# ps.print_stats()
-# print(s.getvalue())
-print(t2-t1)
+print("Took ", round(t2-t1,3), "sec")
 print(r.content)
