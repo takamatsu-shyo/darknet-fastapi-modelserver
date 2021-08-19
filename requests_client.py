539872b20d2c897f43f79924ff36852e33fdc191
Sho Yano <takamatsu.shyo@gmail.com>
2021-08-19 14:24:03 +0300

    Return detections

--- requests_client.py
+++ requests_client.py
@@ -3,7 +3,9 @@ import cv2
 import numpy as np
 import sys
 
-import cProfile, pstats, io
+import cProfile
+import pstats
+import io
 from pstats import SortKey
 
 import time
@@ -14,13 +16,14 @@ import time
 # actual code for profiling
 
 t1 = time.process_time()
-img = cv2.resize(cv2.imread('images/frame4.jpg'), (416,416))
+img = cv2.resize(cv2.imread('images/frame4.jpg'), (416, 416))
 img_encoded = cv2.imencode('.jpg', img)[1]
 img_bytes = img_encoded.tobytes()  # bytes class
 img_hex = img_bytes.hex()
 
-r = requests.post('http://127.0.0.1:8000/predict/', json={"img": img_hex, "dim": (416,416,3), "model_id" : 0, "save_predictions_on_server": True})
+r = requests.post('http://127.0.0.1:8000/predict/', json={"img": img_hex, "dim": (
+    416, 416, 3), "model_id": 0, "save_predictions_on_server": True})
 t2 = time.process_time()
 
-print("Took ", round(t2-t1,3), "sec")
+print("Took ", round(t2-t1, 3), "sec")
 print(r.content)
--- serve.py
+++ serve.py
@@ -9,6 +9,7 @@ import darknet
 
 app = FastAPI()
 
+
 class ModelServer():
     def __init__(self):
         self.nets = {}
@@ -24,9 +25,7 @@ class ModelServer():
                 self.data[model_id] = data = models[model_id]['data']
                 weights = models[model_id]['weights']
 
-
                 netc, classc, colorc = darknet.load_network(cfg, data, weights)
-
 
                 self.nets[model_id] = netc
                 self.class_map[model_id] = classc
@@ -35,51 +34,35 @@ class ModelServer():
             except Exception as e:
                 print(e)
                 print(f"Error: model {model_id} loading failed.")
-    
-    def predict(self, model_id, img_np, img_dim, save_predictions_on_server = False, threshold = 10.):
+
+    def predict(self, model_id, img_np, img_dim, save_predictions_on_server=False, threshold=10.):
         if len(img_dim) == 3:
-            img_for_detect = darknet.make_image(img_dim[0], img_dim[1], img_dim[2])
+            img_for_detect = darknet.make_image(
+                img_dim[0], img_dim[1], img_dim[2])
         elif len(img_dim) == 2:
             img_for_detect = darknet.make_image(img_dim[0], img_dim[1], 1)
         else:
-            raise Exception(f"The image shape is not supported. shape = {img_dim}") 
-        
-        darknet.copy_image_from_bytes(img_for_detect, img_np.tobytes()) 
+            raise Exception(
+                f"The image shape is not supported. shape = {img_dim}")
+
+        darknet.copy_image_from_bytes(img_for_detect, img_np.tobytes())
 
         try:
-            r = darknet.detect_image(self.nets[model_id], self.class_map[model_id], img_for_detect)
-
-            class_labels = []
-            xc = []
-            yc = []
-            w = []
-            h = []
-
-            for ri in r:
-                p = float(ri[1])
-                if p >= threshold:
-                    class_labels.append( self.class_map[model_id].index(ri[0]) )
-                    xc.append( int(ri[2][0])  )
-                    yc.append( int(ri[2][1])  )
-                    w.append( int(ri[2][2])  )
-                    h.append( int(ri[2][3])  )
-
+            detections = darknet.detect_image(
+                self.nets[model_id], self.class_map[model_id], img_for_detect)
             if save_predictions_on_server:
-                pred = darknet.draw_boxes(r, img_np, self.color_map[model_id])
+                pred = darknet.draw_boxes(detections, img_np, self.color_map[model_id])
                 cv2.imwrite(f"predictions/prediction_{model_id}.jpg", pred)
 
-            
-            return {"class_labels": class_labels, "xc": xc, "yc": yc, "w": w, "h": h}
+            return {"detections": detections}
+
         except Exception as e:
             print(e)
             print(f"Error: model {model_id} loading failed.")
-            return {"class_labels": [], "xc": [], "yc": [], "w": [], "h": []}
-        
-
+            return {"detections": ""}
 
 
 app.modelserver = ModelServer()
-
 
 
 class Image(BaseModel):
@@ -104,10 +87,12 @@ def get_model_summary(model_id: int):
 
 @app.post("/predict/")
 def predict(img_model: Image):
-    img_bytes = bytes.fromhex(img_model.img.decode()) # hex to bytes
+    img_bytes = bytes.fromhex(img_model.img.decode())  # hex to bytes
     nparr = np.frombuffer(img_bytes, np.byte)
-    img_np = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED) # decoding this is mendatory, we need a tensor, not a JPEG compressed image file
+    # decoding this is mendatory, we need a tensor, not a JPEG compressed image file
+    img_np = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
 
-    response = app.modelserver.predict(img_model.model_id, img_np, img_model.dim, img_model.save_predictions_on_server)
+    response = app.modelserver.predict(
+        img_model.model_id, img_np, img_model.dim, img_model.save_predictions_on_server)
 
     return response
